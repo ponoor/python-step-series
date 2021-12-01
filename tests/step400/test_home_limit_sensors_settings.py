7dc8caed71b12446ee3e9e5964609fc173d48780
Julian_Orteil <dakota.horstman2001@gmail.com>
2021-12-01 16:41:02 -0600

    Add home and limit sensor settings command tests

---
+++ tests/step400/test_home_limit_sensors_settings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Ensure system commands and responses execute successfully."""
+
+
+import warnings
+
+from stepseries import commands, responses, step400
+from tests.conftest import HardwareIncremental
+
+
+class TestHomeLimitSensorsSettings(HardwareIncremental):
+    def test_home_sw(self, device: step400.STEP400) -> None:
+        device.set(commands.EnableHomeSwReport(3, True))
+        device.set(commands.EnableSwEventReport(3, True))
+        resp = device.get(commands.GetHomeSw(3))
+        assert isinstance(resp, responses.HomeSw)
+
+    def test_limit_sw(self, device: step400.STEP400) -> None:
+        device.set(commands.EnableLimitSwReport(3, True))
+        # TODO: Debug
+        try:
+            resp = device.get(commands.GetLimitSw(3))
+            assert isinstance(resp, responses.LimitSw)
+        except TimeoutError:
+            warnings.warn("test_limit_sw timed-out waiting for a response")
+
+    def test_home_sw_mode(self, device: step400.STEP400) -> None:
+        device.set(commands.SetHomeSwMode(3, 1))
+        resp = device.get(commands.GetHomeSwMode(3))
+        assert isinstance(resp, responses.HomeSwMode)
+        assert resp.swMode == 1
+
+    def test_limit_sw_mode(self, device: step400.STEP400) -> None:
+        device.set(commands.SetLimitSwMode(3, 1))
+        resp = device.get(commands.GetLimitSwMode(3))
+        assert isinstance(resp, responses.LimitSwMode)
+        assert resp.swMode == 1
