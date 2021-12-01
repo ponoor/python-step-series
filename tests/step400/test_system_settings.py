90bceb954587e7909191bb522d147f5351abb209
Julian_Orteil <dakota.horstman2001@gmail.com>
2021-12-01 09:11:31 -0600

    Remove unnecessary import

--- tests/step400/test_system_settings.py
+++ tests/step400/test_system_settings.py
@@ -5,17 +5,17 @@
 
 
 from stepseries import commands, responses, step400
-from tests.conftest import HardwareIncremental, device  # noqa
+from tests.conftest import HardwareIncremental
 
 
 class TestSystemSettings(HardwareIncremental):
-    def test_get_version(self, device: step400.STEP400) -> None:  # noqa
+    def test_get_version(self, device: step400.STEP400) -> None:
         resp = device.get(commands.GetVersion())
         assert isinstance(resp, responses.Version)
 
-    def test_get_config_name(self, device: step400.STEP400) -> None:  # noqa
+    def test_get_config_name(self, device: step400.STEP400) -> None:
         resp = device.get(commands.GetConfigName())
         assert isinstance(resp, responses.ConfigName)
 
-    def test_report_error(self, device: step400.STEP400) -> None:  # noqa
+    def test_report_error(self, device: step400.STEP400) -> None:
         device.set(commands.ReportError(enable=1))
