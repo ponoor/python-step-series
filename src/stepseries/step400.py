9853e971de93fff0b7a95b0493e4e983a37652f4
Julian_Orteil <dakota.horstman2001@gmail.com>
2022-01-17 09:45:04 -0600

    Reduce duplicate code

--- src/stepseries/step400.py
+++ src/stepseries/step400.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""4 axis stepper motor driver with Ethernet interface."""
+"""4 axis stepper motor driver with and Ethernet interface."""
 
 
-from queue import Empty
-from typing import List, Union
-
-from .commands import OSCGetCommand, OSCSetCommand, ReportError, SetDestIP
-from .exceptions import StepSeriesException
-from .responses import OSCResponse, ErrorCommand, ErrorOSC
-from .server import DEFAULT_SERVER
 from .stepXXX import STEPXXX
 
 
@@ -26,7 +19,7 @@ class STEP400(STEPXXX):
             >>> def default_handler(message) -> None:
             ...     print(message)
             ...
-            >>> driver = STEP400(0, '192.168.1.100')
+            >>> driver = STEP400(0, '10.1.21.56')  # Your IP and dip ID here
             >>> driver.on(None, default_handler)
 
     Args:
@@ -46,96 +39,3 @@ class STEP400(STEPXXX):
             Whether to add `id` to `address` and `server_port`
             (the default behavior on the device). Defaults to `True`.
     """
-
-    def get(
-        self, command: OSCGetCommand, with_callback: bool = True
-    ) -> Union[OSCResponse, List[OSCResponse]]:
-        """Send a 'get' command to the device and return the response.
-
-        Note:
-            The responses are also sent to each applicable callback.
-
-            If a `ParseError` is received, then it will be raised. The
-            raw response can be retrieved via the `response` attribute
-            of the error.
-
-        Args:
-            command (`OSCGetCommand`):
-                The completed command template (`stepseries.commands`).
-            with_callback (`bool`):
-                Send the response to callbacks as well
-                (defaults to `True`).
-
-        Raises:
-            `TypeError`:
-                `command` is not an `OSCSetCommand`.
-        """
-
-        if not isinstance(command, OSCGetCommand):
-            raise TypeError(
-                "argument 'command' expected to be 'OSCGetCommand', "
-                f"'{type(command).__name__}' found"
-            )
-
-        self._check_status()
-
-        # Prepare for get request
-        s: str = command.address.replace("get", "")
-        self._get_request = s.lower()
-        self._get_with_callback = with_callback
-        if hasattr(command, "motorID"):
-            if command.motorID == 255:
-                self._is_multiple_response = True
-
-        # Send the request
-        DEFAULT_SERVER.send(self, command)
-
-        # Wait for data and reset
-        try:
-            resp = self._get_queue.get(timeout=2)
-            self._get_queue.task_done()
-        except Empty:
-            raise TimeoutError("timed-out waiting for a response from the device")
-        finally:
-            self._get_request = None
-            self._get_with_callback = True
-            self._multiple_responses = list()
-            self._is_multiple_response = False
-
-        if isinstance(resp, Exception):
-            if isinstance(resp, StepSeriesException):
-                if resp.original_exc is not None:
-                    raise resp from resp.original_exc
-            raise resp
-
-        return resp
-
-    def set(self, command: OSCSetCommand) -> None:
-        """Send a 'set' command to the device.
-
-        Args:
-            command (`OSCCommand`):
-                The completed command template (`stepseries.commands`).
-
-        Raises:
-            `TypeError`:
-                `command` is not an `OSCSetCommand`.
-        """
-
-        if not isinstance(command, OSCSetCommand):
-            raise TypeError(
-                "argument 'command' expected to be 'OSCSetCommand', "
-                f"'{type(command).__name__}' found"
-            )
-
-        if not isinstance(command, SetDestIP):
-            self._check_status()
-
-        if command.__dict__.get("callback", None):
-            if isinstance(command, ReportError):
-                self.on(ErrorCommand, command.callback)
-                self.on(ErrorOSC, command.callback)
-            else:
-                self.on(command.response_cls, command.callback)
-
-        DEFAULT_SERVER.send(self, command)
--- src/stepseries/stepXXX.py
+++ src/stepseries/stepXXX.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""4 axis stepper motor driver with Ethernet interface."""
+"""Stepper motor driver with an Ethernet interface."""
 
 
-from queue import Queue
+from queue import Empty, Queue
 from typing import Any, Callable, Dict, List, Tuple, Union
 
-from .commands import OSCGetCommand, OSCSetCommand, ResetDevice
-from .exceptions import ClientClosedError, ParseError
-from .responses import DestIP, OSCResponse
+from .commands import OSCGetCommand, OSCSetCommand, ReportError, ResetDevice, SetDestIP
+from .exceptions import StepSeriesException, ClientClosedError, ParseError
+from .responses import DestIP, ErrorCommand, ErrorOSC, OSCResponse
 from .server import DEFAULT_SERVER
 
 
@@ -252,9 +252,92 @@ class STEPXXX:
     def get(
         self, command: OSCGetCommand, with_callback: bool = True
     ) -> Union[OSCResponse, List[OSCResponse]]:
-        """Send a 'get' command to the device and return the response."""
-        raise NotImplementedError
+        """Send a 'get' command to the device and return the response.
+
+        Note:
+            The responses are also sent to each applicable callback.
+
+            If a `ParseError` is received, then it will be raised. The
+            raw response can be retrieved via the `response` attribute
+            of the error.
+
+        Args:
+            command (`OSCGetCommand`):
+                The completed command template (`stepseries.commands`).
+            with_callback (`bool`):
+                Send the response to callbacks as well
+                (defaults to `True`).
+
+        Raises:
+            `TypeError`:
+                `command` is not an `OSCSetCommand`.
+        """
+
+        if not isinstance(command, OSCGetCommand):
+            raise TypeError(
+                "argument 'command' expected to be 'OSCGetCommand', "
+                f"'{type(command).__name__}' found"
+            )
+
+        self._check_status()
+
+        # Prepare for get request
+        s: str = command.address.replace("get", "")
+        self._get_request = s.lower()
+        self._get_with_callback = with_callback
+        if hasattr(command, "motorID"):
+            if command.motorID == 255:
+                self._is_multiple_response = True
+
+        # Send the request
+        DEFAULT_SERVER.send(self, command)
+
+        # Wait for data and reset
+        try:
+            resp = self._get_queue.get(timeout=2)
+            self._get_queue.task_done()
+        except Empty:
+            raise TimeoutError("timed-out waiting for a response from the device")
+        finally:
+            self._get_request = None
+            self._get_with_callback = True
+            self._multiple_responses = list()
+            self._is_multiple_response = False
+
+        if isinstance(resp, Exception):
+            if isinstance(resp, StepSeriesException):
+                if resp.original_exc is not None:
+                    raise resp from resp.original_exc
+            raise resp
+
+        return resp
 
     def set(self, command: OSCSetCommand) -> None:
-        """Send a 'set' command to the device."""
-        raise NotImplementedError
+        """Send a 'set' command to the device.
+
+        Args:
+            command (`OSCCommand`):
+                The completed command template (`stepseries.commands`).
+
+        Raises:
+            `TypeError`:
+                `command` is not an `OSCSetCommand`.
+        """
+
+        if not isinstance(command, OSCSetCommand):
+            raise TypeError(
+                "argument 'command' expected to be 'OSCSetCommand', "
+                f"'{type(command).__name__}' found"
+            )
+
+        if not isinstance(command, SetDestIP):
+            self._check_status()
+
+        if command.__dict__.get("callback", None):
+            if isinstance(command, ReportError):
+                self.on(ErrorCommand, command.callback)
+                self.on(ErrorOSC, command.callback)
+            else:
+                self.on(command.response_cls, command.callback)
+
+        DEFAULT_SERVER.send(self, command)
