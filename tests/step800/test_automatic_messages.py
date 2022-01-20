5cc7e279e95d25a202e24decae6d29200f330e28
Julian_Orteil <dakota.horstman2001@gmail.com>
2022-01-20 13:02:15 -0600

    Set booting 800 test to be last

--- tests/step800/test_automatic_messages.py
+++ tests/step800/test_automatic_messages.py
@@ -11,6 +11,7 @@ from stepseries.step800 import STEP800
 
 
 @pytest.mark.skip_800_disconnected
+@pytest.mark.order(-1)
 def test_booted(device: STEP800, wait_for) -> None:
     # Request the device reset and then wait for the response
     wait_for(device, commands.ResetDevice(), responses.Booted)
