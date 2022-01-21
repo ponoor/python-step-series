a5df3f1a65b248692346c70f4d472a55dbd125c5
Julian_Orteil <dakota.horstman2001@gmail.com>
2022-01-21 09:06:58 -0600

    Fix server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -14,7 +14,7 @@ class TestServerOperation:
     def test_add_device(self) -> None:
         device = step400.STEP400(0)
         assert server.DEFAULT_SERVER._bound_devices[-1][0] == device
-        assert server.DEFAULT_SERVER._bound_devices[-1][-1] is None
+        assert server.DEFAULT_SERVER._bound_devices[-1][-1] is not None
 
         device2 = step400.STEP400(0, "10.1.21.56")
         assert server.DEFAULT_SERVER._bound_devices[-1][0] == device2
@@ -25,19 +25,19 @@ class TestServerOperation:
         assert server.DEFAULT_SERVER._bound_devices[-1][-1] is not None
 
     def test_remove_device(self) -> None:
-        device = step400.STEP400(0)
         length = len(server.DEFAULT_SERVER._bound_devices)
-        assert length > 1
+        device = step400.STEP400(0)
+        assert len(server.DEFAULT_SERVER._bound_devices) > length
 
         server.DEFAULT_SERVER.remove_device(device)
-        assert (length - len(server.DEFAULT_SERVER._bound_devices)) == 1
+        assert len(server.DEFAULT_SERVER._bound_devices) == length
 
     def test_shutdown(self) -> None:
         server.DEFAULT_SERVER.shutdown()
         assert len(server.DEFAULT_SERVER._bound_devices) == 0
 
     def test_send_errors(self) -> None:
-        with pytest.raises(exceptions.ClientNotFoundError):
+        with pytest.raises(exceptions.ClientClosedError):
             device = step400.STEP400(0)
             server.DEFAULT_SERVER.remove_device(device)
             device.get(commands.GetVersion())
